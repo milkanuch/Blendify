626d411e8b52e91202554cd20ce0e26a168cc354
Misha Ilkanych <103580682+milkanuch@users.noreply.github.com>
2024-12-07 17:45:38 +0200

    feat: invert button text color (#6)

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -3,8 +3,11 @@ import { Text, Pressable } from "react-native";
 
 import { LinearGradient } from "expo-linear-gradient";
 
+import { getInvertedColor } from "helpers/colors";
+
 import { GRADIENT_END } from "./button.constants";
 
+import { styleSheet } from "./button.styles";
 import { ButtonProps } from "./button.types";
 
 export const Button: FC<ButtonProps> = ({
@@ -16,15 +19,19 @@ export const Button: FC<ButtonProps> = ({
   onPress: handleButtonPress,
   ...rest
 }) => {
+  const textColor = gradientColors && getInvertedColor(gradientColors[0]);
+
+  const styles = styleSheet(textColor);
+
   return (
     <Pressable onPress={handleButtonPress} style={style} {...rest}>
       {gradientColors ? (
         <LinearGradient
           colors={gradientColors}
           end={GRADIENT_END}
-          style={gradientStyle}
+          style={[styles.gradientContainer, gradientStyle]}
         >
-          <Text style={labelStyle}>{label}</Text>
+          <Text style={[styles.label, labelStyle]}>{label}</Text>
         </LinearGradient>
       ) : (
         <Text>{label}</Text>
