3767e05efa80290c6054d6b7ada72d856d06c8fb
Misha Ilkanych <milkanic2@gmail.com>
2024-12-07 14:44:43 +0200

    build: setup gesture handler package

--- App.tsx
+++ App.tsx
@@ -1,14 +1,18 @@
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 import {
   initialWindowMetrics,
   SafeAreaProvider,
 } from "react-native-safe-area-context";
 
 import { HomeScreen } from "./src/screens/HomeScreen";
+import { styles } from "./src/shared/styles";
 
 export const App = () => {
   return (
-    <SafeAreaProvider initialMetrics={initialWindowMetrics}>
-      <HomeScreen />
-    </SafeAreaProvider>
+    <GestureHandlerRootView style={styles.root}>
+      <SafeAreaProvider initialMetrics={initialWindowMetrics}>
+        <HomeScreen />
+      </SafeAreaProvider>
+    </GestureHandlerRootView>
   );
 };
