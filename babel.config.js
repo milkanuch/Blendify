405a631913870ea5c77068bc711b80bf7dcf289b
Misha Ilkanych <milkanic2@gmail.com>
2024-12-07 16:52:48 +0200

    build: add absolute paths

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,21 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ["babel-preset-expo"],
     plugins: [
-      'react-native-reanimated/plugin',
+      [
+        //SOURCE: https://github.com/tleunen/babel-plugin-module-resolver
+        "module-resolver",
+        {
+          alias: {
+            components: "./src/components",
+            screens: "./src/screens",
+            shared: "./src/shared",
+            helpers: "./src/helpers",
+          },
+        },
+      ],
+      "react-native-reanimated/plugin",
     ],
   };
 };
